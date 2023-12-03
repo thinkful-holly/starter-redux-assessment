7addfea2713f4a509733769675f0dd7a7e3040a7
thinkful-holly <146004914+thinkful-holly@users.noreply.github.com>
2023-12-03 13:04:11 -0600

    Update suggestion.slice.js

--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,7 +1,16 @@
+//checked
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */
+    'suggestion/fetchSuggestion', 
+    async (thunkAPI) => {
+      const response = await fetch('http://localhost:3004/api/suggestion').then(
+        (data) => data.json()
+      )
+      console.log('response', response.data);
+      return response.data;
+    });
 
 const initialState = {
   suggestion: '',
@@ -14,7 +23,17 @@ const options = {
   initialState,
   reducers: {},
   extraReducers: {
-    /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+    
+    [fetchSuggestion.pending]: (state) => {state.loading = true}, 
+    [fetchSuggestion.fulfilled]: (state, {payload}) => {
+      state.loading = false;
+      state.suggestion = payload;
+      state.error = false;
+    }, 
+    [fetchSuggestion.rejected]: (state) => {
+      state.loading = false;
+      state.error = true;
+    }
   },
 };
 
@@ -22,7 +41,6 @@ const suggestionSlice = createSlice(options);
 
 export default suggestionSlice.reducer;
 
-// Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
-
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
+export const selectSuggestion = (state) => state.suggestion.suggestion;
