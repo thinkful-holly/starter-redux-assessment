a279f39d122a9aba36ed93d75eacd6ef94aad34b
thinkful-holly <146004914+thinkful-holly@users.noreply.github.com>
2023-12-03 13:01:58 -0600

    Update photos.slice.js

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -1,3 +1,4 @@
+///checked
 import { createSlice } from '@reduxjs/toolkit';
 import { selectSearchTerm } from '../search/search.slice';
 import photos from './photos.data.js';
@@ -13,10 +14,16 @@ const options = {
     // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
     // Task 1 Hint: You can use state.photos.unshift()
     // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+    addPhoto: (state, action) => {
+      state.photos.unshift({ id: state.photos.length + 1, caption: action.payload.caption, imageUrl: action.payload.imageUrl });
+    },
    
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
     // Task 6 Hint: You can use state.photos.splice()
     // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    removePhoto: (state, action) => {
+      state.photos = state.photos.filter(photo => photo.id !== action.payload.id);
+    }
   },
 };
 
@@ -29,4 +36,10 @@ export default photosSlice.reducer;
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
   // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+  const photos = selectAllPhotos(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return photos.filter((photo) =>
+    photo.caption.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 };
