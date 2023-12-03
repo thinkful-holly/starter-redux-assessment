9dd452170e94d1d8403a6cd30e8de0574b0200c7
thinkful-holly <146004914+thinkful-holly@users.noreply.github.com>
2023-12-03 13:03:37 -0600

    Update the suggestion/index.js

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -1,38 +1,44 @@
+///checked
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchSuggestion,
   selectError,
   selectLoading,
-  // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
+  // Task 18: Import the `selectSuggestion()` done
+  selectSuggestion
 } from './suggestion.slice';
 import './suggestion.css';
 
 export default function Suggestion() {
-  // Task 19: Call useSelector() with the selectSuggestion() selector
-  // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  // Task 19: 
+  const { imageUrl, caption } = useSelector(selectSuggestion);
+  console.log('image', imageUrl);
+  // The component needs to access the `imageUrl` and `caption`  done
   const loading = useSelector(selectLoading);
+  console.log('loading', loading);
   const error = useSelector(selectError);
+  console.log('error', error);
   const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadSuggestion() {
-      // Task 20: Dispatch the fetchSuggestion() action creator
+       dispatch(fetchSuggestion());
     }
     loadSuggestion();
   }, [dispatch]);
+
 
   let render;
   if (loading) {
     render = <h3>Loading...</h3>;
   } else if (error) {
-    render = <h3>Sorry, we're having trouble loading the suggestion.</h3>;
+    render = <h3> Sorry, we're having trouble loading the suggestion.</h3>;
   } else {
-    // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
-    render = (
+     render = (
       <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
+        <img alt={caption} src={imageUrl} />
+        <p>{caption}</p>
       </>
     );
   }
